c263cebb873585946f76a6aa1999abb0f081ffaf
azambrana <alvarozs.js@gmal.com>
2013-10-16 21:17:55 -0400

    updated app.js

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ var express = require('express');
 var app = express();
     app.listen(3000);
 
-app.get('/', function(request, response) {
-       response.send("Hello World");
+app.configure(function(){
+    app.use(express.bodyParser());
 });
+
+var routes = require('./routes');
+var data = require('./routes/data');
+
+app.get('/api/data', data.mockup);
+app.get('/index.html', routes.index);
 
 console.log("Application started on localhost:3000");
